d01eb2836599c9ececa399e89115608207d89cff
jeffawheeler23 <jeffawheeler23@gmail.com>
2021-08-28 16:23:47 -0400

    fixed import bug

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/dashboard/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard"
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
   // Set auth token header auth
