b6cdcc57998c997e253a67666e6583324a03152a
jeffawheeler23 <jeffawheeler23@gmail.com>
2021-08-28 04:15:21 -0400

    fixed model import

--- models/User.js
+++ models/User.js
@@ -19,3 +19,5 @@ const UserSchema = new Schema({
     default: Date.now
   }
 });
+
+module.exports = User = mongoose.model("users", UserSchema);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 const app = express();
 // Bodyparser middleware
@@ -19,6 +21,13 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 3000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
